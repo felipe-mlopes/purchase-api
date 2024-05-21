ef529249ab06af8b2de448a5d2af67a21e7b3098
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-05-21 18:21:03 -0300

    fix: change order to array order in promises of the findManyByAuthor and findManyByStatus methods

--- src/domain/purchase/application/repositories/orders-repository.ts
+++ src/domain/purchase/application/repositories/orders-repository.ts
@@ -2,8 +2,8 @@ import { Order, Status } from '../../enterprise/entities/order';
 
 export abstract class OrdersRepository {
   abstract findById(id: string): Promise<Order | null>;
-  abstract findManyByAuthor(authorId: string): Promise<Order | null>;
-  abstract findManyByStatus(status: Status): Promise<Order | null>;
+  abstract findManyByAuthor(authorId: string): Promise<Order[] | null>;
+  abstract findManyByStatus(status: Status): Promise<Order[] | null>;
   abstract save(order: Order): Promise<void>;
   abstract delete(order: Order): Promise<void>;
   abstract create(order: Order): Promise<void>;
