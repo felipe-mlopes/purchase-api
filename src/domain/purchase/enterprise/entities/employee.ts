6ddc53fd9ebe5e925b3b81de01677cd23f0704b3
Felipe Lopes <felipe.mlopes03@gmail.com>
2024-05-20 22:33:43 -0300

    feat: create employee entity

---
+++ src/domain/purchase/enterprise/entities/employee.ts
@@ -0,0 +1,64 @@
+import { Entity } from 'src/core/entities/entity';
+import { UniqueEntityID } from 'src/core/entities/unique-entity-id';
+
+export enum Role {
+  REQUESTER = 'REQUESTER',
+  AUTHORIZER = 'AUTHORIZER',
+  PURCHASER = 'PURCHASER',
+}
+
+interface EmployeeProps {
+  name: string;
+  role: Role;
+  isActive: boolean;
+  email: string;
+  password: string;
+}
+
+export class Employee extends Entity<EmployeeProps> {
+  get name() {
+    return this.props.name;
+  }
+
+  set name(name: string) {
+    this.props.name = name;
+  }
+
+  get role() {
+    return this.props.role;
+  }
+
+  set role(role: Role) {
+    this.props.role = role;
+  }
+
+  get isActive() {
+    return this.props.isActive;
+  }
+
+  set isActive(active: boolean) {
+    this.props.isActive = active;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  set email(email: string) {
+    this.props.email = email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  set password(password: string) {
+    this.props.password = password;
+  }
+
+  static create(props: EmployeeProps, id?: UniqueEntityID) {
+    const employee = new Employee(props, id);
+
+    return employee;
+  }
+}
