91d9af219b24edb84c3691a90cac084ab7c02db6
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-05-22 18:33:42 -0300

    refactor: change findManyByStatus method to findManyRecentByStatus method and include employee id and pagination params as method params

--- src/domain/purchase/application/repositories/orders-repository.ts
+++ src/domain/purchase/application/repositories/orders-repository.ts
@@ -1,9 +1,10 @@
 import { Order, Status } from '../../enterprise/entities/order';
+import { PaginationParams } from 'src/core/repositories/pagination-params';
 
 export abstract class OrdersRepository {
   abstract findById(id: string): Promise<Order | null>;
   abstract findManyByAuthor(authorId: string): Promise<Order[] | null>;
-  abstract findManyByStatus(status: Status): Promise<Order[] | null>;
+  abstract findManyRecentByStatus(employeeId: string, status: Status, params?: PaginationParams): Promise<Order[] | null>;
   abstract save(order: Order): Promise<void>;
   abstract delete(order: Order): Promise<void>;
   abstract create(order: Order): Promise<void>;
