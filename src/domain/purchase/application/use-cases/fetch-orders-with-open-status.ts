1cff34d72efdcf800a8cde9f368fecd56202a3b8
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-05-22 18:39:28 -0300

    refactor: add employee id and page on resquest props, include employees repository dependency and change findManyByStatus method to findManyRecentByStatus method

--- src/domain/purchase/application/use-cases/fetch-orders-with-open-status.ts
+++ src/domain/purchase/application/use-cases/fetch-orders-with-open-status.ts
@@ -1,4 +1,5 @@
 import { OrdersRepository } from '../repositories/orders-repository';
+import { EmployeesRepository } from '../repositories/employees-repository';
 
 import { Order, Status } from '../../enterprise/entities/order';
 import { Role } from '../../enterprise/entities/employee';
@@ -8,7 +9,9 @@ import { NotAllowedError } from 'src/core/errors/not-allowed-error';
 import { ResourceNotFoundError } from 'src/core/errors/resource-not-found-error';
 
 interface FetchOrdersWithOpenStatusRequest {
+  employeeId: string;
   employeeRole: Role;
+  page: number;
 }
 
 type FetchOrdersWithOpenStatusResponse = Either<
@@ -19,18 +22,28 @@ type FetchOrdersWithOpenStatusResponse = Either<
 >;
 
 export class FetchOrdersWithOpenStatus {
-  constructor(private ordersRepository: OrdersRepository) {}
+  constructor(private ordersRepository: OrdersRepository, private employeesRepository: EmployeesRepository,) {}
 
   async execute({
+    employeeId,
     employeeRole,
+    page,
   }: FetchOrdersWithOpenStatusRequest): Promise<FetchOrdersWithOpenStatusResponse> {
     if (employeeRole !== Role.AUTHORIZER || Role.PURCHASER) {
       return left(new NotAllowedError());
     }
 
+    const employee = await this.employeesRepository.findById(employeeId);
+
+    if (!employee) return left(new NotAllowedError());
+
     const statusOpen = Status.OPEN;
 
-    const orders = await this.ordersRepository.findManyByStatus(statusOpen);
+    const orders = await this.ordersRepository.findManyRecentByStatus(
+      employeeId,
+      statusOpen,
+      { page },
+    );
 
     if (!orders) return left(new ResourceNotFoundError());
 
