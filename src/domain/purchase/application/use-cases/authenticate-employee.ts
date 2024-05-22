eddcb3a10ee469e0f11fae3af8b68997b06407d5
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-05-22 16:38:33 -0300

    feat: create authenticate employee use case

---
+++ src/domain/purchase/application/use-cases/authenticate-employee.ts
@@ -0,0 +1,52 @@
+import { EmployeesRepository } from '../repositories/employees-repository';
+import { HashComparer } from '../../cryptography/hash-comparer';
+import { Encrypter } from '../../cryptography/encrypter';
+
+import { Either, left, right } from 'src/core/either';
+import { WrongCredentialsError } from './errors/wrong-credentials-error';
+
+interface AuthenticateEmployeeUsecaseRequest {
+  email: string;
+  password: string;
+}
+
+type AuthenticateEmployeeUsecaseResponse = Either<
+  WrongCredentialsError,
+  {
+    accessToken: string;
+  }
+>;
+
+export class AuthenticateEmployeeUseCase {
+  constructor(
+    private employeesRepository: EmployeesRepository,
+    private hashComparer: HashComparer,
+    private encrypter: Encrypter,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateEmployeeUsecaseRequest): Promise<AuthenticateEmployeeUsecaseResponse> {
+    const employee = await this.employeesRepository.findByEmail(email);
+
+    if (!employee) return left(new WrongCredentialsError());
+
+    const isPasswordValid = await this.hashComparer.compare(
+      password,
+      employee.password,
+    );
+
+    if (!isPasswordValid) return left(new WrongCredentialsError());
+
+    const accessToken = await this.encrypter.encrypt({
+      sub: employee.id.toString(),
+      name: employee.name,
+      role: employee.role,
+    });
+
+    return right({
+      accessToken,
+    });
+  }
+}
