a080c3dcf81cd23bc7913adc92d3623bce5963df
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-06-19 22:07:15 +0000

    test: remove role import path, update should be able authenticate to should be able to authenticate an employee in first test and include not be able to authenticate an employee without registration unit test and not be able to authenticate an employee without registration unit test

--- src/domain/purchase/application/use-cases/authenticate-employee.spec.ts
+++ src/domain/purchase/application/use-cases/authenticate-employee.spec.ts
@@ -3,8 +3,9 @@ import { AuthenticateEmployeeUseCase } from "./authenticate-employee"
 import { InMemoryEmployeesRepository } from "test/repositories/in-memory-employees-repository"
 import { FakeHasher } from "test/cryptography/fake-hasher"
 import { FakeEncrypter } from "test/cryptography/fake-encrypter"
-import { Role } from "../../enterprise/entities/employee"
 import { makeEmployee } from "test/factories/make-employee"
+
+import { WrongCredentialsError } from "./errors/wrong-credentials-error"
 
 describe('Authenticate Employee', () => {
     let inMemoryEmployeesRepository: InMemoryEmployeesRepository
@@ -19,7 +20,7 @@ describe('Authenticate Employee', () => {
         sut = new AuthenticateEmployeeUseCase(inMemoryEmployeesRepository, fakeHasher, fakeEncrypter)
     })
 
-    it('should be able authenticate ', async () => {
+    it('should be able to authenticate an employee', async () => {
         const employee = makeEmployee({
             email: 'johndoe@example.com',
             password: await fakeHasher.hash('123456'),
@@ -36,5 +37,32 @@ describe('Authenticate Employee', () => {
         expect(result.value).toEqual({
             accessToken: expect.any(String)
         })
+    })
+
+    it('should not be able to authenticate an employee without registration', async () => {
+        const result = await sut.execute({
+            email: 'johndoe@example.com',
+            password: '123456'
+        })
+
+        expect(result.isLeft()).toBe(true);
+        expect(result.value).toBeInstanceOf(WrongCredentialsError)
+    })
+
+    it('should not be able to authenticate an employee with an incompatible password', async () => {
+        const employee = makeEmployee({
+            email: 'johndoe@example.com',
+            password: await fakeHasher.hash('123456'),
+        })
+
+        inMemoryEmployeesRepository.create(employee)
+
+        const result = await sut.execute({
+            email: 'johndoe@example.com',
+            password: 'abc123'
+        })
+
+        expect(result.isLeft()).toBe(true);
+        expect(result.value).toBeInstanceOf(WrongCredentialsError)
     })
 })
