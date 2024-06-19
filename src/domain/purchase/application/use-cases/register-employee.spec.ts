6ca2b50acb6701ba5e6557810f3230552f1bce11
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-06-19 21:42:52 +0000

    test: update Role file import path, move inMemoryEmployeesRepository, fakeHasher and sut variables into describe and add not be able to register employee with email already exists unit test and able to hash employee password upon registration unit test

--- src/domain/purchase/application/use-cases/register-employee.spec.ts
+++ src/domain/purchase/application/use-cases/register-employee.spec.ts
@@ -1,18 +1,21 @@
-import { Role } from '../../enterprise/entities/employee';
 import { RegisterEmployeeUseCase } from './register-employee';
+
+import { Role } from '@/domain/purchase/enterprise/entities/employee';
 
 import { FakeHasher } from 'test/cryptography/fake-hasher';
 import { InMemoryEmployeesRepository } from 'test/repositories/in-memory-employees-repository';
 
-let inMemoryEmployeesRepository: InMemoryEmployeesRepository;
-let fakeHasher: FakeHasher;
-let sut: RegisterEmployeeUseCase;
+import { EmployeeAlreadyExistsError } from './errors/employee-already-exist-error';
 
 describe('Register Employee', () => {
-  beforeEach(() => {
-    inMemoryEmployeesRepository = new InMemoryEmployeesRepository();
-    fakeHasher = new FakeHasher();
-    sut = new RegisterEmployeeUseCase(inMemoryEmployeesRepository, fakeHasher);
+  let inMemoryEmployeesRepository: InMemoryEmployeesRepository;
+  let fakeHasher: FakeHasher;
+  let sut: RegisterEmployeeUseCase;
+
+  beforeEach(async () => {
+    inMemoryEmployeesRepository = new InMemoryEmployeesRepository()
+    fakeHasher = new FakeHasher()
+    sut = new RegisterEmployeeUseCase(inMemoryEmployeesRepository, fakeHasher)
   });
 
   it('should be able to register a new employee', async () => {
@@ -28,4 +31,37 @@ describe('Register Employee', () => {
       employee: inMemoryEmployeesRepository.items[0],
     });
   });
+
+  it('should not be able to register a new employee with email already exists', async () => {
+    await sut.execute({
+      name: 'John Doe',
+      email: 'john-doe@example.com',
+      role: Role.REQUESTER,
+      password: '123456',
+    });
+
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'john-doe@example.com',
+      role: Role.REQUESTER,
+      password: '123456',
+    });
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(EmployeeAlreadyExistsError)
+  })
+
+  it('should be able to hash employee password upon registration', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'john-doe@example.com',
+      role: Role.REQUESTER,
+      password: '123456',
+    });
+
+    const hashedPassword = await fakeHasher.hash('123456')
+
+    expect(result.isRight()).toBe(true);
+    expect(inMemoryEmployeesRepository.items[0].password).toEqual(hashedPassword)
+  })
 });
