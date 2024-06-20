faef9054672aa37d73c42bbb334e5c0d0a82889a
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-06-20 22:14:45 +0000

    refactor: add params from PaginationParams in findManyByAuthor and findManyRecentByStatus method and remove employeeId in findManyRecentByStatus method

--- src/domain/purchase/application/repositories/orders-repository.ts
+++ src/domain/purchase/application/repositories/orders-repository.ts
@@ -3,8 +3,8 @@ import { PaginationParams } from '@/core/repositories/pagination-params';
 
 export abstract class OrdersRepository {
   abstract findById(id: string): Promise<Order | null>;
-  abstract findManyByAuthor(authorId: string): Promise<Order[] | null>;
-  abstract findManyRecentByStatus(employeeId: string, status: Status, params?: PaginationParams): Promise<Order[] | null>;
+  abstract findManyByAuthor(authorId: string, params: PaginationParams): Promise<Order[]>;
+  abstract findManyRecentByStatus(status: Status, params: PaginationParams): Promise<Order[]>;
   abstract save(order: Order): Promise<void>;
   abstract delete(order: Order): Promise<void>;
   abstract create(order: Order): Promise<void>;
