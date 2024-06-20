5d7c6c6f7c76259f9c5c4e3e3645ca13f7fb7184
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-06-20 22:20:11 +0000

    test: update file import paths and include page of the PaginationParams and add slice in orders variables in findManyByAuthor and findManyRecentByStatus methods

--- test/repositories/in-memory-orders-repository.ts
+++ test/repositories/in-memory-orders-repository.ts
@@ -1,5 +1,7 @@
-import { OrdersRepository } from 'src/domain/purchase/application/repositories/orders-repository';
-import { Order, Status } from 'src/domain/purchase/enterprise/entities/order';
+import { PaginationParams } from '@/core/repositories/pagination-params';
+
+import { OrdersRepository } from '@/domain/purchase/application/repositories/orders-repository';
+import { Order, Status } from '@/domain/purchase/enterprise/entities/order';
 
 export class InMemoryOrdersRepository implements OrdersRepository {
   public items: Order[] = [];
@@ -12,20 +14,18 @@ export class InMemoryOrdersRepository implements OrdersRepository {
     return order;
   }
 
-  async findManyByAuthor(authorId: string): Promise<Order[] | null> {
-    const orders = this.items.filter(
-      (item) => item.authorId.toString() === authorId,
-    );
-
-    if (!orders) return null;
+  async findManyByAuthor(authorId: string, { page }: PaginationParams): Promise<Order[]> {
+    const orders = this.items
+      .filter((item) => item.authorId.toString() === authorId)
+      .slice((page - 1) * 10, page * 10)
 
     return orders;
   }
 
-  async findManyRecentByStatus(status: Status): Promise<Order[] | null> {
-    const orders = this.items.filter((item) => item.status === status);
-
-    if (!orders) return null;
+  async findManyRecentByStatus(status: Status, { page }: PaginationParams): Promise<Order[]> {
+    const orders = this.items
+      .filter((item) => item.status === status)
+      .slice((page - 1) * 10, page * 10)
 
     return orders;
   }
