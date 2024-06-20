93b8c8ed93c45fedf073e0bf7cf2db767c829966
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-06-20 21:33:55 +0000

    refactor: update file import paths, change role employee condition and include condition that order author's id is not the same as employee's id

--- src/domain/purchase/application/use-cases/delete-order-by-author.ts
+++ src/domain/purchase/application/use-cases/delete-order-by-author.ts
@@ -1,11 +1,11 @@
 import { OrdersRepository } from '../repositories/orders-repository';
 import { EmployeesRepository } from '../repositories/employees-repository';
 
-import { Role } from '../../enterprise/entities/employee';
+import { Role } from '@/domain/purchase/enterprise/entities/employee';
 
-import { Either, left, right } from 'src/core/either';
-import { NotAllowedError } from 'src/core/errors/not-allowed-error';
-import { ResourceNotFoundError } from 'src/core/errors/resource-not-found-error';
+import { Either, left, right } from '@/core/either';
+import { NotAllowedError } from '@/core/errors/not-allowed-error';
+import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error';
 
 interface DeleteOrderByAuthorUseCaseRequest {
   orderId: string;
@@ -31,17 +31,25 @@ export class DeleteOrderByAuthorUseCase {
     authorId,
     employeeRole,
   }: DeleteOrderByAuthorUseCaseRequest): Promise<DeleteOrderByAuthorUseCaseResponse> {
-    if (employeeRole !== Role.AUTHORIZER || Role.REQUESTER) {
+    if (employeeRole !== Role.AUTHORIZER && employeeRole !== Role.REQUESTER) {
       return left(new NotAllowedError());
     }
 
     const employee = await this.employeesRepository.findById(authorId);
 
-    if (!employee) return left(new NotAllowedError());
+    if (!employee) {
+      return left(new NotAllowedError());
+    }
 
     const order = await this.ordersRepository.findById(orderId);
 
-    if (!order) return left(new ResourceNotFoundError());
+    if (!order) {
+      return left(new ResourceNotFoundError());
+    }
+
+    if (order.authorId !== employee.id) {
+      return left(new NotAllowedError())
+    }
 
     await this.ordersRepository.delete(order);
 
