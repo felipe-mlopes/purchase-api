fc8d295d38be2286cfef86bae43b9661c0b1512f
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-06-20 19:15:11 +0000

    test: create unit tests to create order use case

---
+++ src/domain/purchase/application/use-cases/create-order.spec.ts
@@ -0,0 +1,78 @@
+import { InMemoryEmployeesRepository } from "test/repositories/in-memory-employees-repository"
+import { InMemoryOrdersRepository } from "test/repositories/in-memory-orders-repository"
+import { CreateOrderUseCase } from "./create-order"
+import { makeOrder } from "test/factories/make-order"
+import { makeEmployee } from "test/factories/make-employee"
+import { Role } from "../../enterprise/entities/employee"
+import { NotAllowedError } from "@/core/errors/not-allowed-error"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+
+describe('Create Order', () => {
+    let inMemoryOrdersRepository: InMemoryOrdersRepository
+    let inMemoryEmployeesRepository: InMemoryEmployeesRepository
+    let sut: CreateOrderUseCase
+
+    beforeEach(() => {
+        inMemoryOrdersRepository = new InMemoryOrdersRepository()
+        inMemoryEmployeesRepository = new InMemoryEmployeesRepository()
+        sut = new CreateOrderUseCase(inMemoryOrdersRepository, inMemoryEmployeesRepository)
+    })
+
+    it('should be able to create a new order', async () => {
+        const employee = makeEmployee({
+            role: Role.AUTHORIZER
+        })
+
+        await inMemoryEmployeesRepository.create(employee)
+        
+        const result = await sut.execute({
+            authorId: employee.id.toString(),
+            title: 'order-01',
+            costCenter: 'Maintenance',
+            description: 'Maintenance material',
+            employeeRole: employee.role,
+            link: ''
+        })
+
+        expect(result.isRight()).toBe(true)
+        expect(result.value).toEqual({
+            order: inMemoryOrdersRepository.items[0]
+        })
+    })
+
+    it('should not be able to create an order being an employee buyer', async () => {
+        const employee = makeEmployee({
+            role: Role.PURCHASER
+        })
+
+        await inMemoryEmployeesRepository.create(employee)
+        
+        const result = await sut.execute({
+            authorId: employee.id.toString(),
+            title: 'order-01',
+            costCenter: 'Maintenance',
+            description: 'Maintenance material',
+            employeeRole: employee.role,
+            link: ''
+        })
+
+        expect(result.isLeft()).toBe(true)
+        expect(result.value).toBeInstanceOf(NotAllowedError)
+    })
+
+    it('should not be able to create without employee registration', async () => {
+        const employeeId = new UniqueEntityID()
+
+        const result = await sut.execute({
+            authorId: employeeId.toString(),
+            title: 'order-01',
+            costCenter: 'Maintenance',
+            description: 'Maintenance material',
+            employeeRole: Role.REQUESTER,
+            link: ''
+        })
+
+        expect(result.isLeft()).toBe(true)
+        expect(result.value).toBeInstanceOf(NotAllowedError)
+    })
+})
