8ec09f477263ffe28afb861e657cb7e2937a131b
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-06-20 19:06:43 +0000

    refactor: change src to '@' in file import paths and include export in OrderProps interface

--- src/domain/purchase/enterprise/entities/order.ts
+++ src/domain/purchase/enterprise/entities/order.ts
@@ -1,6 +1,6 @@
-import { Entity } from 'src/core/entities/entity';
-import { UniqueEntityID } from 'src/core/entities/unique-entity-id';
-import { Optional } from 'src/core/types/optional';
+import { Entity } from '@/core/entities/entity';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Optional } from '@/core/types/optional';
 
 export enum Status {
   OPEN = 'OPEN',
@@ -8,7 +8,7 @@ export enum Status {
   DONE = 'DONE',
 }
 
-interface OrderProps {
+export interface OrderProps {
   authorId: UniqueEntityID;
   title: string;
   description: string;
