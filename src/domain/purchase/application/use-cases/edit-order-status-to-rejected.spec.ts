9886723b9eca10853a7536181d09a1e38a340222
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-06-24 20:13:46 +0000

    test: create unit tests to edit order status to rejected use case

---
+++ src/domain/purchase/application/use-cases/edit-order-status-to-rejected.spec.ts
@@ -0,0 +1,81 @@
+import { EditOrderStatusToRejectedUseCase } from "./edit-order-status-to-rejected";
+
+import { InMemoryOrdersRepository } from "test/repositories/in-memory-orders-repository"
+import { makeOrder } from "test/factories/make-order"
+
+import { Role } from '@/domain/purchase/enterprise/entities/employee';
+import { Status } from "@/domain/purchase/enterprise/entities/order";
+
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { NotAllowedError } from "@/core/errors/not-allowed-error"
+import { ResourceNotFoundError } from "@/core/errors/resource-not-found-error"
+
+describe('Edit Order Status to Rejected', () => {
+    let inMemoryOrdersRepository: InMemoryOrdersRepository
+    let sut: EditOrderStatusToRejectedUseCase
+
+    beforeEach(() => {
+        inMemoryOrdersRepository = new InMemoryOrdersRepository()
+        sut = new EditOrderStatusToRejectedUseCase(inMemoryOrdersRepository)
+    })
+
+    it('should be able to edit order status to rejected', async () => {
+        const order = makeOrder({
+            status: Status.OPEN
+        })
+
+        await inMemoryOrdersRepository.create(order)
+
+        const result = await sut.execute({
+            employeeRole: Role.AUTHORIZER,
+            orderId: order.id.toString()
+        })
+
+        expect(result.isRight()).toBe(true)
+        expect(result.value.message).toEqual(expect.any(String))
+    })
+
+    it('should not be able to edit order status not being authorizer employee', async () => {
+        const order = makeOrder({
+            status: Status.OPEN
+        })
+
+        await inMemoryOrdersRepository.create(order)
+
+        const result = await sut.execute({
+            employeeRole: Role.PURCHASER,
+            orderId: order.id.toString()
+        })
+
+        expect(result.isLeft()).toBe(true)
+        expect(result.value).toBeInstanceOf(NotAllowedError)
+    })
+
+    it('should not be able to edit order status without registration', async () => {
+        const orderId = new UniqueEntityID()
+
+        const result = await sut.execute({
+            employeeRole: Role.AUTHORIZER,
+            orderId: orderId.toString()
+        })
+
+        expect(result.isLeft()).toBe(true)
+        expect(result.value).toBeInstanceOf(ResourceNotFoundError)
+    })
+
+    it('should not be able to edit order status when it is not open', async () => {
+        const order = makeOrder({
+            status: Status.DONE
+        })
+
+        await inMemoryOrdersRepository.create(order)
+
+        const result = await sut.execute({
+            employeeRole: Role.AUTHORIZER,
+            orderId: order.id.toString()
+        })
+
+        expect(result.isLeft()).toBe(true)
+        expect(result.value).toBeInstanceOf(NotAllowedError)
+    })
+})
