d6f7090b8a76ae85acd3ea0ba54ae0225615d34f
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-06-25 22:12:11 +0000

    refactor: remove AppController and AppService and include HttpModule, EnvModule and ConfigModule in imports

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import { Env, envSchema } from './env/env';
+
+import { HttpModule } from './http/controllers/http.module';
+import { EnvModule } from './env/env.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (env) => envSchema.parse(env),
+    }), 
+    HttpModule, 
+    EnvModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
