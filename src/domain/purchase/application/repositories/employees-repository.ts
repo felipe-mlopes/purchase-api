f397f457b375a6b7f8ac187cae71cef096b100cf
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-06-27 18:35:45 +0000

    refactor: change email to role in findByRole method param and include findAll method with PaginationParams

--- src/domain/purchase/application/repositories/employees-repository.ts
+++ src/domain/purchase/application/repositories/employees-repository.ts
@@ -1,9 +1,11 @@
-import { Employee } from '@/domain/purchase/enterprise/entities/employee';
+import { Employee, Role } from '@/domain/purchase/enterprise/entities/employee';
+import { PaginationParams } from '@/core/repositories/pagination-params';
 
 export abstract class EmployeesRepository {
   abstract findById(id: string): Promise<Employee | null>;
+  abstract findAll(params: PaginationParams): Promise<Employee[]>;
   abstract findByEmail(email: string): Promise<Employee | null>;
-  abstract findByRole(email: string): Promise<Employee[] | null>;
+  abstract findByRole(role: Role): Promise<Employee[] | null>;
   abstract save(employee: Employee): Promise<void>;
   abstract create(employee: Employee): Promise<void>;
 }
