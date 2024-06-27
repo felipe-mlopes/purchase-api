2c3ae04bd372e0077ff6786b168636f0238ce4b8
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-06-27 19:32:17 +0000

    refactor: remove null in findByRole promise method

--- src/domain/purchase/application/repositories/employees-repository.ts
+++ src/domain/purchase/application/repositories/employees-repository.ts
@@ -5,7 +5,7 @@ export abstract class EmployeesRepository {
   abstract findById(id: string): Promise<Employee | null>;
   abstract findAll(params: PaginationParams): Promise<Employee[]>;
   abstract findByEmail(email: string): Promise<Employee | null>;
-  abstract findByRole(role: Role): Promise<Employee[] | null>;
+  abstract findByRole(role: Role): Promise<Employee[]>;
   abstract save(employee: Employee): Promise<void>;
   abstract create(employee: Employee): Promise<void>;
 }
