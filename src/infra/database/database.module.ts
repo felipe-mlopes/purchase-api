cb54ccdf98dba5fc11c77c3bcdac4d3cd4478e0f
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-07-01 21:43:22 +0000

    feat: include ConfigModule in imports, EmployeesRepository as provide, MongoEmployeesRepository as useClass, OrdersRepository as provide, MongoOrdersRepository as useClass and databaseProviders spread in providers and EmployeesRepository, OrdersRepository and databaseProviders spread in exports

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,5 +1,33 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
-@Module({})
+import { databaseProviders } from "./database.providers";
+
+import { MongoEmployeesRepository } from "./mongoose/repositories/mongo-employees.repository";
+import { MongoOrdersRepository } from "./mongoose/repositories/mongo-orders.repository";
+
+import { EmployeesRepository } from "@/domain/purchase/application/repositories/employees-repository";
+import { OrdersRepository } from "@/domain/purchase/application/repositories/orders-repository";
+
+@Module({
+    imports: [ConfigModule],
+    providers: [
+        {
+          provide: EmployeesRepository,
+          useClass: MongoEmployeesRepository
+        },
+        {
+          provide: OrdersRepository,
+          useClass: MongoOrdersRepository
+
+        },
+        ...databaseProviders 
+    ],
+    exports: [
+      EmployeesRepository,
+      OrdersRepository,
+      ...databaseProviders
+    ],
+})
 
 export class DatabaseModule {}
