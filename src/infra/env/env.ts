badc6326e46a8880484fc5dd453d6cffce710f4d
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-07-01 21:34:35 +0000

    refactor: remove SA_KEY in enSchema object

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -1,8 +1,7 @@
 import { z } from "zod"
 
 export const envSchema = z.object({
-    DATABASE_URL: z.string().url(),
-    SA_KEY: z.string()
+    DATABASE_URL: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
