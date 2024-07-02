60cc9b11c5fa59f522452db0a3d34914b21cfb22
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-07-02 21:45:59 +0000

    refactor: change authorId to author_id, costCenter to cost_center, createdAt to created_at, authorizedAt to authorized_at, rejectedAt to rejected_at, completed_at to completedAt and updatedAt to updated_at in order mongoose schema

--- src/infra/database/mongoose/schemas/order.schema.ts
+++ src/infra/database/mongoose/schemas/order.schema.ts
@@ -8,15 +8,16 @@ enum Status {
 }
 
 export const OrderSchema = new mongoose.Schema({
-    authorId: { type: String, required: true },
+    author_id: { type: String, required: true },
+    author_name: { type: String, required: true },
     title: { type: String, required: true },
     description: { type: String, required: true },
     link: { type: String },
-    costCenter: { type: String, required: true },
+    cost_center: { type: String, required: true },
     status: { type: String, enum: Status, required: true },
-    createdAt: { type: Date, default: Date.now },
-    authorizedAt: { type: Date },
-    rejectedAt: { type: Date },
-    completedAt: { type: Date },
-    updatedAt: { type: Date },
+    created_at: { type: Date, default: Date.now },
+    authorized_at: { type: Date },
+    rejected_at: { type: Date },
+    completed_at: { type: Date },
+    updated_at: { type: Date },
 });
