ee1d681f472b8cb3827aecc2f92ca0f7722c642f
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-07-02 21:43:13 +0000

    refactor: remove PaginationParams, include OrderParams interface, change findManyByAuthor and  findManyRecentByStatus params and add findManyOrders method

--- src/domain/purchase/application/repositories/orders-repository.ts
+++ src/domain/purchase/application/repositories/orders-repository.ts
@@ -1,10 +1,19 @@
 import { Order, Status } from '@/domain/purchase/enterprise/entities/order';
-import { PaginationParams } from '@/core/repositories/pagination-params';
+
+export interface OrderParams {
+  startDate?: Date;
+  endDate?: Date;
+  authorName?: string;
+  status?: Status;
+  costCenter?: string;
+  page?: number;
+}
 
 export abstract class OrdersRepository {
   abstract findById(id: string): Promise<Order | null>;
-  abstract findManyByAuthor(authorId: string, params: PaginationParams): Promise<Order[]>;
-  abstract findManyRecentByStatus(status: Status, params: PaginationParams): Promise<Order[]>;
+  abstract findManyOrders(params: OrderParams): Promise<Order[]>;
+  abstract findManyByAuthor(authorId: string, page: number): Promise<Order[]>;
+  abstract findManyRecentByStatus(status: Status, page: number): Promise<Order[]>;
   abstract save(order: Order): Promise<void>;
   abstract delete(order: Order): Promise<void>;
   abstract create(order: Order): Promise<void>;
