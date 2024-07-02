d3284259ea9cf0ac1e74681feecad7a2e615fc81
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-07-02 21:44:17 +0000

    refactor: change isActive to is_active in employee mongoose schema

--- src/infra/database/mongoose/schemas/employee.schema.ts
+++ src/infra/database/mongoose/schemas/employee.schema.ts
@@ -9,7 +9,8 @@ enum Role {
 export const EmployeeSchema = new mongoose.Schema({
   name: { type: String, required: true },
   role: { type: String, enum: Role, required: true },
-  isActive: { type: Boolean, required: true },
+  is_active: { type: Boolean, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  created_at: { type: Date, default: Date.now() }
 });
