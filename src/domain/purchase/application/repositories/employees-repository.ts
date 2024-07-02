4993f58ac4f763d5f33cfabcf538a64567b2e5d2
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-07-02 21:42:16 +0000

    refactor: remove PaginationParams, include EmployeeParams interface and change findAll and findByRole to findManyEmployees method

--- src/domain/purchase/application/repositories/employees-repository.ts
+++ src/domain/purchase/application/repositories/employees-repository.ts
@@ -1,11 +1,17 @@
 import { Employee, Role } from '@/domain/purchase/enterprise/entities/employee';
-import { PaginationParams } from '@/core/repositories/pagination-params';
+
+export interface EmployeeParams {
+  startDate?: Date;
+  endDate?: Date;
+  role?: Role;
+  isActive?: boolean;
+  page?: number;
+}
 
 export abstract class EmployeesRepository {
   abstract findById(id: string): Promise<Employee | null>;
-  abstract findAll(params: PaginationParams): Promise<Employee[]>;
   abstract findByEmail(email: string): Promise<Employee | null>;
-  abstract findByRole(role: Role): Promise<Employee[]>;
+  abstract findManyEmployees(params: EmployeeParams): Promise<Employee[]>;
   abstract save(employee: Employee): Promise<void>;
   abstract create(employee: Employee): Promise<void>;
 }
