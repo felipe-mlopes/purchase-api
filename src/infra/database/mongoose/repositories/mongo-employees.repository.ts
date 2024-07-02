06d3f187d8fa0eab01da6dbbeb90e2b50de70166
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-07-02 21:47:15 +0000

    refactor: add EmployeeParams, change findAll to findManyEmployees method and implement all methods

--- src/infra/database/mongoose/repositories/mongo-employees.repository.ts
+++ src/infra/database/mongoose/repositories/mongo-employees.repository.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { Model } from "mongoose";
 
-import { EmployeesRepository } from "@/domain/purchase/application/repositories/employees-repository";
+import { EmployeeParams, EmployeesRepository } from "@/domain/purchase/application/repositories/employees-repository";
 import { Employee } from "@/domain/purchase/enterprise/entities/employee";
 
 @Injectable()
@@ -12,27 +12,56 @@ export class MongoEmployeesRepository implements EmployeesRepository {
     ) {}
 
     async findById(id: string): Promise<Employee | null> {
-        throw new Error("Method not implemented.");
-    
+        return await this.employeeModel.findById(id).exec()
     }
 
-    async findAll(): Promise<Employee[]> {
-        throw new Error("Method not implemented.");
+    async findManyEmployees({ startDate, endDate, role, isActive, page }: EmployeeParams): Promise<Employee[]> {
+        const query: any = await this.employeeModel.find()
+
+        if (startDate || endDate) {
+            query.where('created_at');
+
+            if (startDate) {
+                query.gte(startDate.getTime());
+              }
+        
+            if (endDate) {
+                query.lte(endDate.getTime());
+            }
+        }
+
+        if (role) {
+            query.where('role').equals(role)
+        }
+
+        if (isActive !== undefined) {
+            query.where('is_active').equals(isActive)
+        }
+
+        const currentPage = page || 1
+        const limit = 10
+        const skip = (currentPage - 1) * limit
+
+        return query.skip(skip).limit(limit).exec()
     }
 
     async findByEmail(email: string): Promise<Employee | null> {
-        throw new Error("Method not implemented.");
-    }
-
-    async findByRole(email: string): Promise<Employee[]> {
-        throw new Error("Method not implemented.");
+        return await this.employeeModel.findOne({ email, isActive: true }).exec()
     }
 
     async save(employee: Employee): Promise<void> {
-        throw new Error("Method not implemented.");
+        const id = employee.id.toString()
+
+        return await this.employeeModel.findByIdAndUpdate(id, employee).exec()
+    }
+
+    async delete(employee: Employee): Promise<void> {
+        const id = employee.id.toString()
+
+        return await this.employeeModel.findByIdAndDelete(id).exec()
     }
 
     async create(employee: Employee): Promise<void> {
-        throw new Error("Method not implemented.");
+        return await this.employeeModel.create(employee)
     }    
 }
