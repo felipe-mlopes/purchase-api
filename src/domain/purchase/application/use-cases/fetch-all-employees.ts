5a9030fe2246ca0f51d37f24fcf449bae6b4a28b
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-07-04 02:53:35 +0000

    feat: include startDate, endDate, role and isActive on request and findManyEmployees method params

--- src/domain/purchase/application/use-cases/fetch-all-employees.ts
+++ src/domain/purchase/application/use-cases/fetch-all-employees.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
 
-import { Employee } from "@/domain/purchase/enterprise/entities/employee";
+import { Employee, Role } from "@/domain/purchase/enterprise/entities/employee";
 
 import { EmployeesRepository } from "../repositories/employees-repository";
 
@@ -8,6 +8,10 @@ import { Either, right } from "@/core/either";
 import { ResourceNotFoundError } from "@/core/errors/resource-not-found-error";
 
 interface FetchAllEmployeesUseCaseRequest {
+    startDate?: Date;
+    endDate?: Date;
+    role?: Role;
+    isActive?: boolean;
     page: number;
 }
 
@@ -23,9 +27,17 @@ export class FetchAllEmployeesUseCase {
     constructor(private employeeRepository: EmployeesRepository) {}
 
     async execute({
+        startDate,
+        endDate,
+        role,
+        isActive,
         page
     }: FetchAllEmployeesUseCaseRequest): Promise<FetchAllEmployeesUseCaseResponse> {
-        const employees = await this.employeeRepository.findAll({
+        const employees = await this.employeeRepository.findManyEmployees({
+            startDate,
+            endDate,
+            role,
+            isActive,
             page
         })
 
