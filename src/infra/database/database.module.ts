5b8f41eb7fce1a476f77dc4e764e0a464fc6de15
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-07-04 02:57:53 +0000

    refactor: remove imports, change all to  databaseProviders spread in providers and exports

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,33 +1,9 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-
 import { databaseProviders } from "./database.providers";
 
-import { MongoEmployeesRepository } from "./mongoose/repositories/mongo-employees.repository";
-import { MongoOrdersRepository } from "./mongoose/repositories/mongo-orders.repository";
-
-import { EmployeesRepository } from "@/domain/purchase/application/repositories/employees-repository";
-import { OrdersRepository } from "@/domain/purchase/application/repositories/orders-repository";
-
 @Module({
-    imports: [ConfigModule],
-    providers: [
-        {
-          provide: EmployeesRepository,
-          useClass: MongoEmployeesRepository
-        },
-        {
-          provide: OrdersRepository,
-          useClass: MongoOrdersRepository
-
-        },
-        ...databaseProviders 
-    ],
-    exports: [
-      EmployeesRepository,
-      OrdersRepository,
-      ...databaseProviders
-    ],
+    providers: [...databaseProviders],
+    exports: [...databaseProviders],
 })
 
 export class DatabaseModule {}
