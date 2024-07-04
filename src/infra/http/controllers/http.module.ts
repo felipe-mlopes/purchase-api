9190f2fdc620bb895c0d801a4d133641cb44a732
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-07-04 02:59:24 +0000

    refactor: remove GetEmployeeByIdController in controllers and GetEmployeeByIdUseCase in providers

--- src/infra/http/controllers/http.module.ts
+++ src/infra/http/controllers/http.module.ts
@@ -2,18 +2,10 @@ import { Module } from "@nestjs/common";
 
 import { DatabaseModule } from "@/infra/database/database.module";
 
-import { GetEmployeeByIdController } from "./get-employee-by-id.controller";
-
-import { GetEmployeeByIdUseCase } from "@/domain/purchase/application/use-cases/get-employee-by-id";
-
 @Module({
     imports: [DatabaseModule],
-    controllers: [
-        GetEmployeeByIdController
-    ],
-    providers: [
-        GetEmployeeByIdUseCase
-    ]
+    controllers: [],
+    providers: []
 })
 
 export class HttpModule {}
